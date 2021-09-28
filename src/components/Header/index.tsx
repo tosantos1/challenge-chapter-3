196fbd88fe295316c014ea58657cd9f9aae4c5a6
Tiago Oliveira <tiagolivesan@outlook.com>
2021-09-28 18:08:00 -0300

    Upgrade index, header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
+import styles from './header.module.scss';
+
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <a href="/" >
+          <img src="/images/Logo.svg" alt="logo" />
+        </a>
+      </div>
+    </header>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from 'next';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { FiCalendar, FiUser } from "react-icons/fi";
+
 
 interface Post {
   uid?: string;
@@ -24,9 +27,20 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <main className={styles.mainContainer}>
+      <div className={styles.mainContent}>
+        <h1>Como utilizar Hooks</h1>
+        <p>Pesando em sincronização em vez de clicos de vida.</p>
+        <div className={styles.datamember}>
+          <p><FiCalendar /> <span> 15 Mar 2021 </span></p>
+          <p><FiUser /> <span> Tiago Oliveira </span></p>
+        </div>
+      </div>
+    </main>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
